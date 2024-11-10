13fd45747a5d0b7c43bffa7f07533c8b3a4bb4fd
Inbestigator <inbestigatoryt@gmail.com>
2024-11-10 12:40:50 -0800

    Test certs

--- main.test.ts
+++ main.test.ts
@@ -1,14 +1,21 @@
 import nacl from "tweetnacl";
 import { verifySignature } from "./lib/core/server.ts";
+import { Buffer } from "node:buffer";
+import { assertEquals } from "@std/assert";
 
 function generateXSignature(timestamp: string) {
   const keyPair = nacl.sign.keyPair();
-  const message = new TextEncoder().encode("asdasd" + timestamp);
-  const signature = nacl.sign.detached(message, keyPair.secretKey).toString();
-  return signature.slice(0, 128);
+  const message = new TextEncoder().encode(timestamp + "content");
+  const signature = nacl.sign.detached(message, keyPair.secretKey);
+
+  Deno.env.set(
+    "DISCORD_PUBLIC_KEY",
+    Buffer.from(keyPair.publicKey).toString("hex"),
+  );
+  return Buffer.from(signature).toString("hex");
 }
 
-Deno.test("Invalid signature", async () => {
+Deno.test("Don't verify invalid signature", async () => {
   const stamp = Date.now().toString();
   const result = await verifySignature(
     new Request("http://localhost:8000", {
@@ -17,10 +24,25 @@ Deno.test("Invalid signature", async () => {
         "X-Signature-Ed25519": generateXSignature(stamp),
         "X-Signature-Timestamp": stamp,
       },
-      body: "asdasd",
+      body: "different content",
     }),
   );
-  if (result) {
-    throw new Error("Should not verify signature");
-  }
+
+  assertEquals(result, false);
+});
+
+Deno.test("Verify valid signature", async () => {
+  const stamp = Date.now().toString();
+  const result = await verifySignature(
+    new Request("http://localhost:8000", {
+      method: "POST",
+      headers: {
+        "X-Signature-Ed25519": generateXSignature(stamp),
+        "X-Signature-Timestamp": stamp,
+      },
+      body: "content",
+    }),
+  );
+
+  assertEquals(result, true);
 });
