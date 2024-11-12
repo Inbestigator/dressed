94acb7c899d31d997d815df02631dc79488a80f9
Inbestigator <inbestigatoryt@gmail.com>
2024-11-11 19:35:31 -0800

    Removed create-new

--- lib/exports/cmd.ts
+++ lib/exports/cmd.ts
@@ -1,8 +1,6 @@
 import loader from "../internal/loader.ts";
-import { dirname, join } from "node:path";
 import { Command } from "commander";
 import { createInstance } from "../core/instance.ts";
-import { green, italic } from "@std/fmt/colors";
 import { build } from "./mod.ts";
 
 const program = new Command();
@@ -28,78 +26,6 @@ program
     const outputContent = await build(instance);
     Deno.writeFileSync("./bot.gen.ts", new TextEncoder().encode(outputContent));
     loader("Wrote to bot.gen.ts").resolve();
-  });
-
-program
-  .command("create-new")
-  .description("Create a new HTTP bot.")
-  .argument("[name]", "Project name")
-  .option("-t, --token <token>", "Bot token")
-  .option("-s, --simple", "Use a simple template")
-  .action((name, { token, simple }) => {
-    if (!name) {
-      name = prompt("Project name:");
-    }
-    if (!name) {
-      console.log("Project name cannot be empty.");
-      return;
-    }
-    if (!token) {
-      token = prompt("Bot token (optional):");
-    }
-    if (simple === undefined) {
-      simple = confirm("Do you want to use a simple template?");
-    }
-    const mkdirLoader = loader(`Creating files for project: ${name}`);
-
-    async function createFiles(path: string, dest: string) {
-      Deno.mkdirSync(dest, { recursive: true });
-      const response = await fetch(path);
-
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-
-      const json = await response.json();
-      if (Array.isArray(json)) {
-        for (const file of json) {
-          if (file.type === "dir") {
-            createFiles(file.url, join(dest, file.name));
-          } else {
-            const fileRes = await fetch(file.download_url);
-            if (!fileRes.ok) {
-              throw new Error(fileRes.statusText);
-            }
-            let fileContents = await fileRes.text();
-            let destPath = join(dest, file.name);
-            if (file.name === ".env.example" && token) {
-              fileContents =
-                `DISCORD_TOKEN="${token}"\nDISCORD_APP_ID=""\nDISCORD_PUBLIC_KEY=""`;
-              destPath = join(dest, ".env");
-            }
-            Deno.mkdirSync(dirname(destPath), { recursive: true });
-            Deno.writeTextFileSync(destPath, fileContents);
-          }
-        }
-      }
-    }
-
-    try {
-      const path = simple
-        ? "https://api.github.com/repos/Inbestigator/http-discord/contents/templates/simple"
-        : "https://api.github.com/repos/Inbestigator/http-discord/contents/templates/base";
-
-      createFiles(path, join(Deno.cwd(), name));
-    } catch {
-      mkdirLoader.error();
-      throw new Error("Failed to create files");
-    }
-    mkdirLoader.resolve();
-
-    console.log(
-      green("Project created successfully."),
-      italic("\n$ deno task bot dev"),
-    );
   });
 
 program.parse();
