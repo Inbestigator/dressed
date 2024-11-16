e16795a45c48ebb3a80dd2234212ee294c594e23
Inbestigator <inbestigatoryt@gmail.com>
2024-11-15 21:17:19 -0800

    Simplified interaction types

--- lib/internal/types/interaction.ts
+++ lib/internal/types/interaction.ts
@@ -9,38 +9,41 @@ import type {
 } from "discord-api-types/v10";
 import type { EditMessageOptions, MessageComponents } from "./messages.ts";
 
+interface BaseInteractionMethods {
+  reply: (data: InteractionReplyOptions) => Promise<void>;
+  deferReply: (data?: DeferredReplyOptions) => Promise<void>;
+  editReply: (data: EditMessageOptions) => Promise<void>;
+  followUp: (data: InteractionReplyOptions) => Promise<void>;
+  user: APIUser;
+}
+
 /**
  * A command interaction, includes methods for responding to the interaction.
  */
-export type CommandInteraction = APIApplicationCommandInteraction & {
-  reply: (data: InteractionReplyOptions) => Promise<void>;
-  deferReply: (data?: DeferredReplyOptions) => Promise<void>;
-  showModal: (data: unknown) => Promise<void>;
-  editReply: (data: EditMessageOptions) => Promise<void>;
-  followUp: (data: InteractionReplyOptions) => Promise<void>;
-};
+export type CommandInteraction =
+  & APIApplicationCommandInteraction
+  & BaseInteractionMethods
+  & {
+    showModal: (data: unknown) => Promise<void>;
+  };
 
 /**
  * A message component interaction, includes methods for responding to the interaction.
  */
-export type MessageComponentInteraction = APIMessageComponentInteraction & {
-  reply: (data: InteractionReplyOptions) => Promise<void>;
-  deferReply: (data: DeferredReplyOptions) => Promise<void>;
-  showModal: (data: unknown) => Promise<void>;
-  update: (data: EditMessageOptions) => Promise<void>;
-  editReply: (data: EditMessageOptions) => Promise<void>;
-  followUp: (data: InteractionReplyOptions) => Promise<void>;
-};
+export type MessageComponentInteraction =
+  & APIMessageComponentInteraction
+  & BaseInteractionMethods
+  & {
+    showModal: (data: unknown) => Promise<void>;
+    update: (data: EditMessageOptions) => Promise<void>;
+  };
 
 /**
  * A modal submit interaction, includes methods for responding to the interaction.
  */
-export type ModalSubmitInteraction = APIModalSubmitInteraction & {
-  reply: (data: InteractionReplyOptions) => Promise<void>;
-  deferReply: (data: DeferredReplyOptions) => Promise<void>;
-  editReply: (data: EditMessageOptions) => Promise<void>;
-  followUp: (data: InteractionReplyOptions) => Promise<void>;
-};
+export type ModalSubmitInteraction =
+  & APIModalSubmitInteraction
+  & BaseInteractionMethods;
 
 export interface DeferredReplyOptions {
   ephemeral?: boolean;
@@ -57,11 +60,8 @@ export type InteractionReplyOptions =
   }
   | string;
 
-export type Interaction<T extends APIInteraction> =
-  & (T extends APIApplicationCommandInteraction ? CommandInteraction
-    : T extends APIMessageComponentInteraction ? MessageComponentInteraction
-    : T extends APIModalSubmitInteraction ? ModalSubmitInteraction
-    : null)
-  & {
-    user: APIUser;
-  };
+export type Interaction<T extends APIInteraction> = T extends
+  APIApplicationCommandInteraction ? CommandInteraction
+  : T extends APIMessageComponentInteraction ? MessageComponentInteraction
+  : T extends APIModalSubmitInteraction ? ModalSubmitInteraction
+  : null;
