bcb0f4788d013773550c361029b44e54129b15de
Inbestigator <inbestigatoryt@gmail.com>
2025-04-12 03:00:35 -0700

    Assert type

--- tests/options.test.ts
+++ tests/options.test.ts
@@ -1,41 +1,41 @@
-// import { assertEquals } from "@std/assert";
-// import type { APIApplicationCommandInteraction } from "discord-api-types/v10";
-// import createInteraction from "../lib/internal/interaction.ts";
+import { assertEquals } from "@std/assert";
+import type { APIApplicationCommandInteraction } from "discord-api-types/v10";
+import createInteraction from "../lib/internal/interaction.ts";
 
-// const command = {
-//   type: 2,
-//   data: {
-//     options: [
-//       {
-//         name: "option1",
-//         type: 5,
-//         value: true,
-//       },
-//     ],
-//   },
-// } as APIApplicationCommandInteraction;
-// const interaction = createInteraction(command);
+const command = {
+  type: 2,
+  data: {
+    options: [
+      {
+        name: "option1",
+        type: 5,
+        value: true,
+      },
+    ],
+  },
+} as APIApplicationCommandInteraction;
+const interaction = createInteraction(command);
 
-// Deno.test("Check for existing option", () => {
-//   assertEquals(
-//     interaction.getOption("option1", true).boolean(),
-//     true,
-//   );
-//   assertEquals(
-//     interaction.getOption("option1")?.boolean(),
-//     true,
-//   );
-// });
+Deno.test("Check for existing option", () => {
+  assertEquals(
+    interaction.getOption("option1", true).boolean(),
+    true,
+  );
+  assertEquals(
+    interaction.getOption("option1")?.boolean(),
+    true,
+  );
+});
 
-// Deno.test("Check for non existing option", () => {
-//   try {
-//     interaction.getOption("option2", true);
-//     throw new Error("Should have thrown");
-//   } catch {
-//     // pass
-//   }
-//   assertEquals(
-//     interaction.getOption("option2")?.boolean(),
-//     undefined,
-//   );
-// });
+Deno.test("Check for non existing option", () => {
+  try {
+    interaction.getOption("option2", true);
+    throw new Error("Should have thrown");
+  } catch {
+    // pass
+  }
+  assertEquals(
+    interaction.getOption("option2")?.boolean(),
+    undefined,
+  );
+});
