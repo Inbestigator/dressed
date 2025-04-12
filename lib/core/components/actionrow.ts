a62d46aad20a9cee10abb0565deae876302b9045
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2025-04-12 03:03:19 -0700

    Changed actionrow strictness and made select return more specifically (#39)

--- lib/core/components/actionrow.ts
+++ lib/core/components/actionrow.ts
@@ -1,8 +1,7 @@
 import {
   type APIActionRowComponent,
+  type APIActionRowComponentTypes,
   type APIButtonComponent,
-  type APISelectMenuComponent,
-  type APITextInputComponent,
   ComponentType,
 } from "discord-api-types/v10";
 
@@ -12,22 +11,13 @@ import {
  * Provide a list of buttons, a select menu, or a text input
  */
 export function ActionRow<
-  T extends
-    | APIButtonComponent[]
-    | [APISelectMenuComponent]
-    | [APITextInputComponent],
+  T extends APIActionRowComponentTypes,
 >(
-  ...components: T
-): APIActionRowComponent<Component<T>> {
+  ...components: T extends APIButtonComponent ? APIButtonComponent[]
+    : [APIActionRowComponentTypes]
+): APIActionRowComponent<T> {
   return {
-    components: components as Component<T>[],
+    components: components as T[],
     type: ComponentType.ActionRow,
   };
 }
-
-type Component<T> = T extends APIButtonComponent[] ? APIButtonComponent
-  : T extends [
-    APISelectMenuComponent,
-  ] ? APISelectMenuComponent
-  : T extends [APITextInputComponent] ? APITextInputComponent
-  : never;
