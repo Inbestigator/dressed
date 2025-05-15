fd38df990375706763322aadd54ef36bfbf1860f
Inbestigator <inbestigatoryt@gmail.com>
2025-05-15 01:48:59 -0700

    docs: update and jsdoc

--- packages/dressed-next/src/index.ts
+++ packages/dressed-next/src/index.ts
@@ -10,15 +10,15 @@ import { waitUntil } from "@vercel/functions";
 /**
  * Creates a request handler for Next.js routes that processes commands, components, and events.
  *
- * @param commands - Array of command data to process
- * @param components - Array of component data to process
- * @param events - Array of event data to process
+ * @param commands - Array of command datas to use
+ * @param components - Array of component datas to use
+ * @param events - Array of event datas to use
  * @returns A request handler function compatible with Next.js API routes
  */
 export function createHandler(
-  commands: CommandData[],
-  components: ComponentData[],
-  events: EventData[],
+  commands: CommandData<"ext">[],
+  components: ComponentData<"ext">[],
+  events: EventData<"ext">[],
 ): (req: Request) => Promise<Response> {
   const [runCommand, runComponent, runEvent] = [
     setupCommands(commands),
