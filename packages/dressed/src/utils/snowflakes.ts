0c45e78948bd0d97d481e8b20b449235b211f314
Inbestigator <inbestigatoryt@gmail.com>
2025-07-28 05:32:43 -0700

    refactor: move decoded into return object

--- packages/dressed/src/utils/snowflakes.ts
+++ packages/dressed/src/utils/snowflakes.ts
@@ -19,9 +19,8 @@ export function encodeSnowflake({
   process,
   increment,
 }: DecodedSnowflake): Snowflake {
-  const timePart = BigInt(timestamp) - DISCORD_EPOCH;
   const snowflake =
-    (timePart << BigInt(22)) |
+    ((BigInt(timestamp) - DISCORD_EPOCH) << BigInt(22)) |
     (BigInt(worker & 0b11111) << BigInt(17)) |
     (BigInt(process & 0b11111) << BigInt(12)) |
     BigInt(increment & 0xfff);
@@ -31,14 +30,10 @@ export function encodeSnowflake({
 /** Deserialize the contents of a snowflake */
 export function decodeSnowflake(snowflake: Snowflake): DecodedSnowflake {
   const id = BigInt(snowflake);
-  const timestamp = Number((id >> BigInt(22)) + DISCORD_EPOCH);
-  const worker = Number((id & BigInt(0x3e0000)) >> BigInt(17));
-  const process = Number((id & BigInt(0x1f000)) >> BigInt(12));
-  const increment = Number(id & BigInt(0xfff));
   return {
-    timestamp,
-    worker,
-    process,
-    increment,
+    timestamp: Number((id >> BigInt(22)) + DISCORD_EPOCH),
+    worker: Number((id & BigInt(0x3e0000)) >> BigInt(17)),
+    process: Number((id & BigInt(0x1f000)) >> BigInt(12)),
+    increment: Number(id & BigInt(0xfff)),
   };
 }
