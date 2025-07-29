12414d09a95dd638aea46302902416e0460d4dd4
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2025-07-28 23:08:28 -0700

    fix: don't redefine filetype, useless if stm (#133)

--- packages/dressed/src/types/interaction.ts
+++ packages/dressed/src/types/interaction.ts
@@ -20,29 +20,7 @@ import type {
   getOption,
   OptionValueGetters,
 } from "../server/extenders/options.ts";
-import type { Buffer } from "node:buffer";
-
-/** Type ripped from Discord.js (tysm) */
-interface RawFile {
-  /**
-   * Content-Type of the file
-   */
-  contentType?: string;
-  /**
-   * The actual data for the file
-   */
-  data: Buffer | Uint8Array | boolean | number | string;
-  /**
-   * An explicit key to use for key of the formdata field for this file.
-   * When not provided, the index of the file in the files array is used in the form `files[${index}]`.
-   * If you wish to alter the placeholder snowflake, you must provide this property in the same form (`files[${placeholder}]`)
-   */
-  key?: string;
-  /**
-   * The name of the file
-   */
-  name: string;
-}
+import type { RawFile } from "./file.ts";
 
 /**
  * A command interaction, includes methods for responding to the interaction.
