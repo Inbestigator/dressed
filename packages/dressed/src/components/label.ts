5f7894ebf9dcb0c1bb3e3e783849bf831092d922
Inbestigator <inbestigatoryt@gmail.com>
2025-08-27 17:09:13 -0700

    feat(component): modal updates

---
+++ packages/dressed/src/components/label.ts
@@ -0,0 +1,51 @@
+// TODO
+// Import label types from discord-api-types once they merge modal components
+// Use ComponentType.Label instead of hardcoding 18
+// Remove input check
+import {
+  ComponentType,
+  type APIStringSelectComponent,
+  type APITextInputComponent,
+} from "discord-api-types/v10";
+
+type APIComponentInLabel =
+  | (APIStringSelectComponent & { required?: boolean })
+  | Omit<APITextInputComponent, "label">;
+
+interface APILabelComponent {
+  id?: number;
+  /** The label text */
+  label: string;
+  /** An optional description textfor the label */
+  description?: string;
+  /** The component within the label */
+  component: APIComponentInLabel;
+  type: 18;
+}
+
+/**
+ * Creates a label component
+ *
+ * Container associating a label and description with a component
+ */
+export function Label(
+  label: string,
+  component: APIComponentInLabel,
+  description?: string,
+  config?: Omit<
+    APILabelComponent,
+    "label" | "component" | "description" | "type"
+  >,
+): APILabelComponent {
+  if (component.type === ComponentType.TextInput) {
+    // @ts-expect-error No labels for inputs in labels.
+    delete component.label;
+  }
+  return {
+    ...config,
+    label,
+    description,
+    component,
+    type: 18,
+  };
+}
