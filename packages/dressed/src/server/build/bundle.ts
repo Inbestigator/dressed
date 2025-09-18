10f4474aad3e42e173334a0c5b57b6b60dffb12b
Inbestigator <inbestigatoryt@gmail.com>
2025-09-18 08:31:32 -0700

    refactor: check for bun in process

--- packages/dressed/src/server/build/bundle.ts
+++ packages/dressed/src/server/build/bundle.ts
@@ -1,5 +1,5 @@
 export default async function bundleFiles(entry: string, outdir: string) {
-  try {
+  if (process.versions.bun) {
     const { build } = await import("bun");
     return build({
       entrypoints: [entry],
@@ -9,26 +9,27 @@ export default async function bundleFiles(entry: string, outdir: string) {
       target: "bun",
       packages: "external",
     });
+  }
+  try {
+    const { build } = await import("esbuild");
+    return build({
+      entryPoints: [entry],
+      outdir,
+      outExtension: { ".js": ".mjs" },
+      bundle: true,
+      minify: true,
+      splitting: true,
+      platform: "node",
+      format: "esm",
+      write: true,
+      treeShaking: true,
+      jsx: "automatic",
+      logLevel: "error",
+      packages: "external",
+    });
   } catch {
-    try {
-      const { build } = await import("esbuild");
-      return build({
-        entryPoints: [entry],
-        outdir,
-        outExtension: { ".js": ".mjs" },
-        bundle: true,
-        minify: true,
-        splitting: true,
-        platform: "node",
-        format: "esm",
-        write: true,
-        treeShaking: true,
-        jsx: "automatic",
-        logLevel: "error",
-        packages: "external",
-      });
-    } catch {
-      throw new Error("You must install esbuild to be able to bundles files outside of a Bun environment");
-    }
+    throw new Error(
+      "Dressed requires Esbuild to bundle files, but it is not installed. Please install 'esbuild' and try again.",
+    );
   }
 }
