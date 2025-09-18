4f92bf0fe990dee8c8c448ada532e6bd0ec9a1c9
Inbestigator <inbestigatoryt@gmail.com>
2025-09-17 21:55:40 -0700

    refactor(bundle): make esbuild a peer dep

--- packages/dressed/src/server/build/bundle.ts
+++ packages/dressed/src/server/build/bundle.ts
@@ -1,19 +1,34 @@
-import { build } from "esbuild";
-
 export default async function bundleFiles(entry: string, outdir: string) {
-  await build({
-    entryPoints: [entry],
-    outdir,
-    outExtension: { ".js": ".mjs" },
-    bundle: true,
-    minify: true,
-    splitting: true,
-    platform: "node",
-    format: "esm",
-    write: true,
-    treeShaking: true,
-    jsx: "automatic",
-    logLevel: "error",
-    packages: "external",
-  });
+  try {
+    const { build } = await import("bun");
+    return build({
+      entrypoints: [entry],
+      outdir,
+      naming: "[dir]/[name].mjs",
+      minify: true,
+      target: "bun",
+      packages: "external",
+    });
+  } catch {
+    try {
+      const { build } = await import("esbuild");
+      return build({
+        entryPoints: [entry],
+        outdir,
+        outExtension: { ".js": ".mjs" },
+        bundle: true,
+        minify: true,
+        splitting: true,
+        platform: "node",
+        format: "esm",
+        write: true,
+        treeShaking: true,
+        jsx: "automatic",
+        logLevel: "error",
+        packages: "external",
+      });
+    } catch {
+      throw new Error("You must install esbuild to be able to bundles files outside of a Bun environment");
+    }
+  }
 }
