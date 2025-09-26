9499c928a5c8cda287dd306081b79fee99ccfb85
Inbestigator <inbestigatoryt@gmail.com>
2025-09-25 22:04:46 -0700

    refactor: no need to replace env (run once)

--- packages/dressed/src/utils/dotenv.ts
+++ packages/dressed/src/utils/dotenv.ts
@@ -13,71 +13,47 @@ export type LoadedEnvFiles = Array<{
   env: Env;
 }>;
 
-let initialEnv: Env;
-let cachedLoadedEnvFiles: LoadedEnvFiles = [];
-
-function replaceProcessEnv(sourceEnv: Env) {
-  Object.keys(process.env).forEach((key) => {
-    if (!key.startsWith("__NEXT_PRIVATE")) {
-      if (sourceEnv[key] === undefined || sourceEnv[key] === "") {
-        delete process.env[key];
-      }
-    }
-  });
-
-  Object.entries(sourceEnv).forEach(([key, value]) => {
-    process.env[key] = value;
-  });
-}
+const initialEnv: Env = Object.assign({}, process.env);
 
 function processEnv(loadedEnvFiles: LoadedEnvFiles) {
-  if (!initialEnv) {
-    initialEnv = Object.assign({}, process.env);
-  }
-  if (process.env.__NEXT_PROCESSED_ENV || loadedEnvFiles.length === 0) {
+  if (process.env.__PROCESSED_ENV || loadedEnvFiles.length === 0) {
     return process.env as Env;
   }
-  process.env.__NEXT_PROCESSED_ENV = "true";
+  process.env.__PROCESSED_ENV = "true";
 
-  const origEnv = Object.assign({}, initialEnv);
   const parsed: DotenvParseOutput = {};
 
   for (const envFile of loadedEnvFiles) {
     try {
       const result = parse(envFile.contents);
       for (const key of Object.keys(result)) {
-        if (typeof parsed[key] === "undefined" && typeof origEnv[key] === "undefined") {
+        if (typeof parsed[key] === "undefined" && typeof initialEnv[key] === "undefined") {
           parsed[key] = result[key];
         }
       }
       envFile.env = result;
     } catch {}
   }
-  return Object.assign(process.env, parsed);
+  Object.assign(process.env, parsed);
 }
 
 export function loadEnvConfig() {
-  if (!initialEnv) {
-    initialEnv = Object.assign({}, process.env);
-  }
-  replaceProcessEnv(initialEnv);
-  cachedLoadedEnvFiles = [];
-
   const isTest = process.env.NODE_ENV === "test";
-  const mode = isTest ? "test" : process.env.NODE_ENV === "development" ? "development" : "production";
+  const isDev = process.env.NODE_ENV === "development";
+  const mode = isTest ? "test" : isDev ? "development" : "production";
   const dotenvFiles = [`.env.${mode}.local`, mode !== "test" && `.env.local`, `.env.${mode}`, ".env"].filter(
     Boolean,
   ) as string[];
+  const files = [];
 
   for (const envFile of dotenvFiles) {
     const dotEnvPath = join(".", envFile);
     try {
       const stats = statSync(dotEnvPath);
       if (!stats.isFile() && !stats.isFIFO()) continue;
-
       const contents = readFileSync(dotEnvPath, "utf8");
-      cachedLoadedEnvFiles.push({ path: envFile, contents, env: {} });
+      files.push({ path: envFile, contents, env: {} });
     } catch {}
   }
-  processEnv(cachedLoadedEnvFiles);
+  processEnv(files);
 }
