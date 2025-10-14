07d5c045aac92503eb0564783e53b9cce19634e3
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2025-10-14 11:12:18 -0700

    fix(dry-publish): remove slow types (#193)

--- packages/dressed/src/server/build/parsers/commands.ts
+++ packages/dressed/src/server/build/parsers/commands.ts
@@ -1,10 +1,8 @@
 import type { CommandData } from "../../../types/config.ts";
 import { createHandlerParser } from "./index.ts";
 
-export const parseCommands = createHandlerParser<CommandData>({
+export const parseCommands: ReturnType<typeof createHandlerParser<CommandData>> = createHandlerParser({
   colNames: ["Command"],
-  itemMessages: ({ name }) => ({
-    confict: `"${name}" conflicts with another command, skipping the duplicate`,
-  }),
+  itemMessages: ({ name }) => ({ confict: `"${name}" conflicts with another command, skipping the duplicate` }),
   createData: () => ({}),
 });
