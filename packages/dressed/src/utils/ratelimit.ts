e520353263ba8aaa1ce9a19c4202f1afad2de87e
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2025-10-24 00:30:53 -0700

    fix(ratelimit): non-inherent bucket crossover lead to unflushed reqs (#199)

--- packages/dressed/src/utils/ratelimit.ts
+++ packages/dressed/src/utils/ratelimit.ts
@@ -2,8 +2,8 @@ interface Bucket {
   remaining: number;
   limit: number;
   queue: (() => void)[];
+  refresh: number;
   sweeper?: NodeJS.Timeout;
-  tmp?: true;
 }
 
 const buckets = new Map<string, Bucket>();
@@ -21,13 +21,13 @@ function ensureBucket(id: string) {
   return (
     buckets.get(bucketId) ??
     // biome-ignore lint/style/noNonNullAssertion: We're setting, so it's guaranteed
-    buckets.set(bucketId, { limit: 1, remaining: 1, queue: [], tmp: true }).get(bucketId)!
+    buckets.set(bucketId, { limit: 1, remaining: 1, queue: [], refresh: -1 }).get(bucketId)!
   );
 }
 
 export function checkLimit(req: Request) {
   const bucket = ensureBucket(`${req.method}:${req.url}`);
-  if (bucket.remaining-- > 0 && bucket.tmp) return checkGlobalLimit();
+  if (bucket.remaining-- > 0 && bucket.refresh < Date.now()) return checkGlobalLimit();
   return new Promise<void>((r) => bucket.queue.push(r));
 }
 
@@ -40,10 +40,12 @@ export function updateLimit(req: Request, res: Response) {
   const scope = res.headers.get("x-ratelimit-scope");
   const id = `${req.method}:${req.url}`;
   const tmpBucket = buckets.get(id);
+  const now = Date.now();
+  const refreshAfter = (Number.isNaN(retryAfter) ? resetAfter : retryAfter) * 1000;
   buckets.delete(id);
 
   if (scope === "global" && !Number.isNaN(retryAfter)) {
-    globalReset = Date.now() + retryAfter * 1000;
+    globalReset = now + retryAfter * 1000;
     return;
   }
   if ([limit, remaining, resetAfter].some(Number.isNaN) || !bucketId) return;
@@ -67,15 +69,13 @@ export function updateLimit(req: Request, res: Response) {
     limit,
     remaining,
     queue: (bucket?.queue ?? []).concat(tmpBucket?.queue ?? []),
-    sweeper: setTimeout(
-      () => {
-        const bucket = buckets.get(bucketId);
-        if (!bucket || globalReset > Date.now()) return;
-        bucket.remaining = bucket.limit;
-        release();
-      },
-      (Number.isNaN(retryAfter) ? resetAfter : retryAfter) * 1000,
-    ),
+    refresh: now + refreshAfter,
+    sweeper: setTimeout(() => {
+      const bucket = buckets.get(bucketId);
+      if (!bucket || globalReset > Date.now()) return;
+      bucket.remaining = bucket.limit;
+      release();
+    }, refreshAfter),
   });
   if (remaining > 0) release();
 }
