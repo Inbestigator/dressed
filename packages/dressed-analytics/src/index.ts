94db248901ad30ca246b08620d2a8e8d9b396fc9
Inbestigator <inbestigatoryt@gmail.com>
2026-01-03 04:52:29 -0800

    refactor: use middleware fn for patching

--- packages/dressed-analytics/src/index.ts
+++ packages/dressed-analytics/src/index.ts
@@ -1,59 +1,78 @@
-import type { APIChatInputApplicationCommandInteraction } from "discord-api-types/v10";
-import { createInteraction, createServer } from "dressed/server";
+import { createInteraction, createServer, type ServerConfig } from "dressed/server";
 import { serverConfig } from "dressed/utils";
 
 function log(key: string, data) {
   console.log(key, data);
 }
 
-function wrapMethods<T>(target: T, methods: string[], callback: (method: string, args: unknown[]) => void) {
-  for (const method of methods as (keyof T)[]) {
-    const original = target[method];
-    if (typeof original !== "function") continue;
+// function wrapMethods<T>(target: T, methods: string[], callback: (method: string, args: unknown[]) => void) {
+//   for (const method of methods as (keyof T)[]) {
+//     const original = target[method];
+//     if (typeof original !== "function") continue;
 
-    target[method] = ((...args: unknown[]) => {
-      callback(String(method), args);
-      return original(...args);
-    }) as T[typeof method];
-  }
+//     target[method] = ((...args: unknown[]) => {
+//       callback(String(method), args);
+//       return original(...args);
+//     }) as T[typeof method];
+//   }
+// }
+
+// export function patchInteraction<T extends NonNullable<ReturnType<typeof createInteraction>>>(interaction: T): T {
+//   log(`interaction:${interaction.id}`, {
+//     guildId: interaction.guild_id,
+//     channelId: interaction.channel?.id,
+//     userId: interaction.user.id,
+//     type: interaction.type,
+//   });
+//   wrapMethods(
+//     interaction,
+//     ["reply", "deferReply", "update", "deferUpdate", "editReply", "followUp", "showModal", "sendChoices"],
+//     (method, [first]) => {
+//       log(`response:${method}`, first);
+//     },
+//   );
+
+//   wrapMethods(interaction, ["getOption", "getValues", "getField"], (method, [first]) => {
+//     log(`data-access:${method}`, { accessed: first });
+//   });
+
+//   return interaction;
+// }
+
+function mw<T extends unknown[], R>(next?: (...p: T) => R) {
+  return (...p: T) => {
+    const interaction = p[0] as NonNullable<ReturnType<typeof createInteraction>>;
+    log(`interaction:${interaction.id}`, {
+      guildId: interaction.guild_id,
+      channelId: interaction.channel?.id,
+      userId: interaction.user.id,
+      type: interaction.type,
+    });
+    return next?.(...p) ?? p;
+  };
 }
 
-export function patchInteraction<T extends NonNullable<ReturnType<typeof createInteraction>>>(interaction: T): T {
-  log(`interaction:${interaction.id}`, {
-    guildId: interaction.guild_id,
-    channelId: interaction.channel?.id,
-    userId: interaction.user.id,
-    type: interaction.type,
-  });
-  wrapMethods(
-    interaction,
-    ["reply", "deferReply", "update", "deferUpdate", "editReply", "followUp", "showModal", "sendChoices"],
-    (method, [first]) => {
-      log(`response:${method}`, first);
-    },
-  );
-
-  wrapMethods(interaction, ["getOption", "getValues", "getField"], (method, [first]) => {
-    log(`data-access:${method}`, { accessed: first });
-  });
-
-  return interaction;
+function analyticsMiddleware(
+  next: ServerConfig["middleware"] = {},
+): Required<Pick<NonNullable<ServerConfig["middleware"]>, "commands" | "components">> &
+  Pick<NonNullable<ServerConfig["middleware"]>, "events"> {
+  return {
+    commands: mw(next.commands),
+    components: mw(next.components),
+    events: next.events,
+  };
 }
-
-const a = patchInteraction(createInteraction({} as APIChatInputApplicationCommandInteraction));
 
 serverConfig.port = 3000;
 serverConfig.requests = {
   // routeBase: "http://localhost:3000",
   env: {},
 };
-serverConfig.middleware = {
+
+serverConfig.middleware = analyticsMiddleware({
   commands(i) {
-    console.log(patchInteraction(i));
     return [i];
   },
-};
+});
 
 // createServer([{ data: {}, exports: { default() {} }, name: "test", path: "", uid: "" }], [], []);
-
-a.getOption("wasd");
