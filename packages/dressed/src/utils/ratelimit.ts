072bd90c6bef1e289bc861a8ae41836f58f1b685
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2026-01-07 15:51:26 -0800

    feat(ratelimit): combine sequential patch requests (#217)

--- packages/dressed/src/utils/ratelimit.ts
+++ packages/dressed/src/utils/ratelimit.ts
@@ -4,7 +4,8 @@ interface Bucket {
   remaining: number;
   limit: number;
   refresh: number;
-  promise: Promise<void>;
+  queued: Request[];
+  promise: Promise<number>;
   cleaner?: NodeJS.Timeout;
 }
 
@@ -20,12 +21,16 @@ function ensureBucket(id: string) {
       limit: 1,
       remaining: 1,
       refresh: -1,
-      promise: Promise.resolve(),
+      queued: [],
+      promise: Promise.resolve(0),
     });
   }
   const bucket = buckets.get(bucketId) as Bucket;
   // Runtimes like CF workers persist global state between requests except promises -> null
-  bucket.promise ??= Promise.resolve();
+  if (bucket.promise === null) {
+    bucket.promise = Promise.resolve(0);
+    bucket.queued = [];
+  }
 
   return bucket;
 }
@@ -42,10 +47,13 @@ const cleanup = (bucketId: string) => () => {
 };
 
 export function checkLimit(req: Request, bucketTTL: number) {
-  return new Promise<(v: Response) => void>((resolveChecker) => {
+  return new Promise<[(v: Response) => void, Request]>((resolveChecker) => {
     const bucketIdKey = `${req.method}:${req.url}`;
     const bucket = ensureBucket(bucketIdKey);
-    bucket.promise = bucket.promise.then(async () => {
+    bucket.queued.push(req);
+    bucket.promise = bucket.promise.then(async (r) => {
+      if (r-- > 0) return r;
+
       const deltaG = globalReset - Date.now();
 
       if (deltaG > 0) {
@@ -60,46 +68,98 @@ export function checkLimit(req: Request, bucketTTL: number) {
         bucket.remaining = bucket.limit - 1;
       }
 
-      let resolveRequest: () => void;
+      let resolveRequest: (n: number) => void;
 
-      resolveChecker((res) => {
-        const bucketId = res.headers.get("x-ratelimit-bucket");
-        const limit = Number.parseInt(res.headers.get("x-ratelimit-limit") ?? "", 10);
-        const remaining = Number.parseInt(res.headers.get("x-ratelimit-remaining") ?? "", 10);
-        const resetAfter = Number.parseFloat(res.headers.get("x-ratelimit-reset-after") ?? "");
-        const retryAfter = Number.parseFloat(res.headers.get("retry-after") ?? "");
-        const scope = res.headers.get("x-ratelimit-scope");
-        const refreshAfter = (Number.isNaN(retryAfter) ? resetAfter : retryAfter) * 1000;
-        const tmpBucket = buckets.get(bucketIdKey);
+      const { next, combined } = req.method === "PATCH" ? await combineReqs(bucket.queued) : { next: req, combined: 1 };
+      bucket.queued = bucket.queued.slice(combined);
 
-        buckets.delete(bucketIdKey);
+      resolveChecker([
+        (res) => {
+          const bucketId = res.headers.get("x-ratelimit-bucket");
+          const limit = Number.parseInt(res.headers.get("x-ratelimit-limit") ?? "", 10);
+          const remaining = Number.parseInt(res.headers.get("x-ratelimit-remaining") ?? "", 10);
+          const resetAfter = Number.parseFloat(res.headers.get("x-ratelimit-reset-after") ?? "");
+          const retryAfter = Number.parseFloat(res.headers.get("retry-after") ?? "");
+          const scope = res.headers.get("x-ratelimit-scope");
+          const refreshAfter = (Number.isNaN(retryAfter) ? resetAfter : retryAfter) * 1000;
+          const tmpBucket = buckets.get(bucketIdKey);
 
-        if (scope === "global" && !Number.isNaN(retryAfter)) {
-          globalReset = Date.now() + retryAfter * 1000;
-          return resolveRequest();
-        }
-        if ([limit, remaining, resetAfter].some(Number.isNaN) || !bucketId) return resolveRequest();
+          buckets.delete(bucketIdKey);
 
-        bucketIds.set(bucketIdKey, bucketId);
+          if (scope === "global" && !Number.isNaN(retryAfter)) {
+            globalReset = Date.now() + retryAfter * 1000;
+            return resolveRequest(combined - 1);
+          }
+          if ([limit, remaining, resetAfter].some(Number.isNaN) || !bucketId) return resolveRequest(combined - 1);
 
-        const bucket = ensureBucket(bucketIdKey);
+          bucketIds.set(bucketIdKey, bucketId);
 
-        bucket.limit = limit;
-        bucket.remaining = remaining;
-        bucket.refresh = Date.now() + refreshAfter;
-        bucket.promise = tmpBucket ? bucket.promise.then(() => tmpBucket.promise) : bucket.promise; // NOSONAR
+          const bucket = ensureBucket(bucketIdKey);
 
-        clearTimeout(bucket.cleaner);
-        if (bucketTTL !== -1) {
-          bucket.cleaner = setTimeout(cleanup(bucketId), bucketTTL * 1000).unref();
-        }
+          bucket.limit = limit;
+          bucket.remaining = remaining;
+          bucket.refresh = Date.now() + refreshAfter;
+          bucket.promise = tmpBucket ? bucket.promise.then(() => tmpBucket.promise) : bucket.promise; // NOSONAR
+          bucket.queued = tmpBucket ? bucket.queued.concat(tmpBucket.queued) : bucket.queued;
 
-        resolveRequest();
-      });
+          clearTimeout(bucket.cleaner);
+          if (bucketTTL !== -1) {
+            bucket.cleaner = setTimeout(cleanup(bucketId), bucketTTL * 1000).unref();
+          }
 
-      return new Promise<void>((r) => {
+          resolveRequest(combined - 1);
+        },
+        next,
+      ]);
+
+      return new Promise<number>((r) => {
         resolveRequest = r;
       });
     });
   });
+}
+
+async function combineReqs(reqs: Request[]): Promise<{ next: Request; combined: number }> {
+  const group: Request[] = [];
+
+  const payload = {};
+  let lastForm: FormData | undefined;
+
+  for (const r of reqs) {
+    if (r.headers.get("authorization") !== reqs[0].headers.get("authorization") || r.url !== reqs[0].url) break;
+
+    group.push(r);
+
+    const ct = r.headers.get("content-type") ?? "";
+
+    if (ct.includes("multipart/form-data") || r.body instanceof FormData) {
+      lastForm = await r.clone().formData();
+      const jsonPayload = lastForm.get("payload_json");
+      if (jsonPayload) {
+        Object.assign(payload, JSON.parse(jsonPayload as string));
+      }
+    } else {
+      Object.assign(payload, await r.clone().json());
+    }
+  }
+
+  const body = lastForm ?? JSON.stringify(payload);
+
+  const { headers } = group[0];
+
+  if (lastForm) {
+    lastForm.set("payload_json", JSON.stringify(payload));
+    headers.delete("content-type");
+  } else {
+    headers.set("content-type", "application/json");
+  }
+
+  return {
+    next: new Request(group[0].url, {
+      method: group[0].method,
+      headers,
+      body,
+    }),
+    combined: group.length,
+  };
 }
