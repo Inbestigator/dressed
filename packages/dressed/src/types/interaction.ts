65d8e3b811058b3f1c66d0fa31d95d52e28730ae
Inbestigator <inbestigatoryt@gmail.com>
2026-02-01 15:51:53 -0800

    refactor: place global commands under app and remove depr routes

--- packages/dressed/src/types/interaction.ts
+++ packages/dressed/src/types/interaction.ts
@@ -193,8 +193,6 @@ export type ModalSubmitInteraction = APIModalSubmitInteraction &
      * Get a field from the user's submission
      * @param custom_id The custom_id of the field
      * @param required Whether the field is required
-     *
-     * **The returned string is deprecated, use `.textInput()` to fetch the value of a text input in the future**
      */
     getField: <R extends boolean>(custom_id: string, required?: R) => ReturnType<typeof getField<R>>;
   };
