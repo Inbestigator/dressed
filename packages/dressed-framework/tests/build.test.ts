cedc90e942e2262a72cb3bef97d47465ce5e8696
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2026-02-23 23:01:12 -0800

    Discard changes to packages/dressed-framework/tests/build.test.ts

--- packages/dressed-framework/tests/build.test.ts
+++ packages/dressed-framework/tests/build.test.ts
@@ -3,6 +3,5 @@ import build from "@dressed/framework/build";
 
 test("Build bot", async () => {
   const result = await build({ build: { root: "tests/src" } });
-  console.log(process.env.__PROCESSED_ENV);
   expect(result).toMatchSnapshot();
 });
