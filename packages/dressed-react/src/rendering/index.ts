83a17e83db8a7c4b7ddae2b48147a3b5d37054c7
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2026-03-22 06:00:13 -0700

    docs(dressed-react): update render fn docs (#253)

--- packages/dressed-react/src/rendering/index.ts
+++ packages/dressed-react/src/rendering/index.ts
@@ -2,6 +2,11 @@ import type { ReactNode } from "react";
 import { reconciler } from "../react/reconciler.ts";
 import { createRenderer, type RendererCallback } from "../react/renderer.ts";
 
+/**
+ * Renders a component tree.
+ * @param children The components to render
+ * @param callback A function called each time the React process updates the components
+ */
 export function render(children: ReactNode, callback: RendererCallback) {
   const renderer = createRenderer(callback);
 
