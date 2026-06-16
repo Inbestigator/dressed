ec68e283e261c19f74c7d77b0c13acb50a0cb76a
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2026-06-15 23:17:58 -0700

    feat(resources): additional api routes (#227)

--- packages/dressed/src/resources/make/index.ts
+++ packages/dressed/src/resources/make/index.ts
@@ -52,6 +52,7 @@ ${Object.entries(routeDefinitions)
           apiRoute,
           dataType,
           dangerousExtraLogic,
+          dangerousReturnLogic,
           name,
           keyNameStart,
           returnType,
@@ -112,11 +113,11 @@ export async function ${name}${generic ? `<${generic}>` : ""}(${params
     .map((p) => (p.includes(":") ? p.split(/[?:]/)[0] : p).replace(/botEnv\.([A-Z_]+)/, "$req?.env?.$1??$&"))}), {
       ${[`method: "${method.toUpperCase()}"`, params.some((p) => p.startsWith("data")) && "body: data", params.some((p) => p.startsWith("params")) && "params", flags?.includes("hasFiles") && `files: ${fileValue}`].filter(Boolean)}
   }, $req);
-  ${flags?.includes("returnVoid") ? "" : "return res.json()"}
+  ${dangerousReturnLogic || (flags?.includes("returnVoid") ? "" : "return res.json()")}
 }
 `
           .trim()
-          .replace(/\/docs/g, "https://discord.com/developers$&"))();
+          .replace(/\/docs/g, "https://docs.discord.com/developers"))();
     },
   )
   .join("\n\n")}
