1bcdcc0f824a3821ebd88d5bac26d904f03b8144
Inbestigator <inbestigatoryt@gmail.com>
2026-07-02 14:51:43 -0700

    refactor(signature): use buffer.from instead of naive

--- packages/dressed/src/server/signature.test.ts
+++ packages/dressed/src/server/signature.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "bun:test";
+import { beforeAll, expect, test } from "bun:test";
 import { botEnv } from "../utils/env.ts";
 import { verifySignature } from "./signature.ts";
 
@@ -7,14 +7,16 @@ const publicKey = "66a27ecb1443a21f793f55fb80b2a9a8335bcd0b8421d1a5fa21491aeb1fe
 const signature =
   "92b5b884aaf24a4eda897eb3e0dddcadfd94883938edaaef7664933b91cf613668ebd7a7b691778004f1e15259243ce2cc83527f10a71154028f9b407dbca20a";
 
-test("Don't verify invalid signature", () => {
-  botEnv.DISCORD_PUBLIC_KEY = publicKey;
-  const result = verifySignature("different test", signature, stamp);
-  expect(result).resolves.toBeFalse();
+beforeAll(() => (botEnv.DISCORD_PUBLIC_KEY = publicKey));
+
+test("returns true for a valid signature", () => {
+  expect(verifySignature("test", signature, stamp)).resolves.toBeTrue();
 });
 
-test("Verify valid signature", () => {
-  botEnv.DISCORD_PUBLIC_KEY = publicKey;
-  const result = verifySignature("test", signature, stamp);
-  expect(result).resolves.toBeTrue();
+test("returns false when the request body is modified", () => {
+  expect(verifySignature("different test", signature, stamp)).resolves.toBeFalse();
+});
+
+test("returns false when the timestamp is modified", () => {
+  expect(verifySignature("test", signature, "1")).resolves.toBeFalse();
 });
--- packages/dressed/src/server/signature.ts
+++ packages/dressed/src/server/signature.ts
@@ -1,21 +1,13 @@
 import { botEnv } from "../utils/env.ts";
 
-function hex2bin(hex: string) {
-  const buf = new Uint8Array(Math.ceil(hex.length / 2));
-  for (let i = 0; i < buf.length; ++i) {
-    buf[i] = Number.parseInt(hex.substring(i * 2, i * 2 + 2), 16);
-  }
-  return buf;
-}
-
 /**
  * Verifies the signature of the POST request
  */
 export async function verifySignature(body: string, signature: string, timestamp: string): Promise<boolean> {
   return crypto.subtle.verify(
     { name: "Ed25519" },
-    await crypto.subtle.importKey("raw", hex2bin(botEnv.DISCORD_PUBLIC_KEY), "Ed25519", false, ["verify"]),
-    hex2bin(signature),
+    await crypto.subtle.importKey("raw", Buffer.from(botEnv.DISCORD_PUBLIC_KEY, "hex"), "Ed25519", false, ["verify"]),
+    Buffer.from(signature, "hex"),
     new TextEncoder().encode(timestamp + body),
   );
 }
